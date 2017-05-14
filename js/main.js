e895874675e28436da14f77960b666b514bca9ad
Computer <computer@192-168-1-111.tpgi.com.au>
2017-05-14 18:12:07 +1000

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+ var cardOne = "queen";
+ var cardTwo = "queen";
+ var cardThree = "king";
+ var cardFour = "king";
+
+console.log("User flipper" + cardOne);
+console.log("User flipper" + cardFour);
